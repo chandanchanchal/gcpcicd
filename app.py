20c2af4c46539c59dd1a25d3f9b1b5fa62dee535
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-09-24 09:19:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello All FOR CICD"
+    return "Hello All FOR CICD with githubActions"
 
 if __name__ == "__main__":
     # Cloud Run requires listening on 0.0.0.0 and the PORT env var
